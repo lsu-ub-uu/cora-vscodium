133d89f33246a06081babf87c2543cbe65e41833
Olov McKie <olov.mckie@ub.uu.se>
2023-09-08 13:46:47 +0000

    better command to install plugins

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -48,7 +48,7 @@ setupWithRecommendedData(){
 	mkdir ~/vscodium/vscodiumforcora/data
 
 	echo "Starting VSCodium for the first time to create folder structure in data"
-	~/vscodium/vscodiumforcora/codium --enable-features=UseOzonePlatform --ozone-platform=wayland --no-sandbox
+	~/vscodium/vscodiumforcora/bin/codium --enable-features=UseOzonePlatform --ozone-platform=wayland --no-sandbox
 
 	echo "Moving settings.json"
 	mv ~/data/settings.json ~/vscodium/vscodiumforcora/data/user-data/User/
@@ -65,30 +65,30 @@ installExtensions(){
 	# ~/vscodium/vscodiumforcora/bin/codium --no-sandbox --list-extensions --show-versions
 	
 	echo "Installing recommended extensions"
-	~/vscodium/vscodiumforcora/codium --force --install-extension BriteSnow.vscode-toggle-quotes@0.3.6
-	~/vscodium/vscodiumforcora/codium --force --install-extension genuitecllc.codetogether@2023.1.1
+	~/vscodium/vscodiumforcora/bin/codium --no-sandbox --force --install-extension BriteSnow.vscode-toggle-quotes@0.3.6
+	~/vscodium/vscodiumforcora/bin/codium --no-sandbox --force --install-extension genuitecllc.codetogether@2023.1.1
 
-	~/vscodium/vscodiumforcora/codium --force --install-extension dbaeumer.vscode-eslint@2.4.2
-	~/vscodium/vscodiumforcora/codium --force --install-extension eamodio.gitlens@13.5.0
-	~/vscodium/vscodiumforcora/codium --force --install-extension esbenp.prettier-vscode@10.1.0
-	~/vscodium/vscodiumforcora/codium --force --install-extension formulahendry.auto-close-tag@0.5.14
-	~/vscodium/vscodiumforcora/codium --force --install-extension formulahendry.auto-rename-tag@0.1.10
-	~/vscodium/vscodiumforcora/codium --force --install-extension humao.rest-client@0.25.1
-	~/vscodium/vscodiumforcora/codium --force --install-extension jeff-hykin.better-dockerfile-syntax@1.0.2
-	~/vscodium/vscodiumforcora/codium --force --install-extension jeff-hykin.better-shellscript-syntax@1.6.2
-	~/vscodium/vscodiumforcora/codium --force --install-extension maciekkoks.luvia-theme@0.1.24
-	~/vscodium/vscodiumforcora/codium --force --install-extension MartinJohns.inline-types@0.3.0
-	~/vscodium/vscodiumforcora/codium --force --install-extension mhutchie.git-graph@1.30.0
-	~/vscodium/vscodiumforcora/codium --force --install-extension ms-azuretools.vscode-docker@1.24.0
-	~/vscodium/vscodiumforcora/codium --force --install-extension naumovs.color-highlight@2.5.0
-	~/vscodium/vscodiumforcora/codium --force --install-extension Nixon.env-cmd-file-syntax@0.3.0
-	~/vscodium/vscodiumforcora/codium --force --install-extension PKief.material-icon-theme@4.30.1
-	~/vscodium/vscodiumforcora/codium --force --install-extension SonarSource.sonarlint-vscode@3.21.0
-	~/vscodium/vscodiumforcora/codium --force --install-extension wix.vscode-import-cost@3.3.0
+	~/vscodium/vscodiumforcora/bin/codium --no-sandbox --force --install-extension dbaeumer.vscode-eslint@2.4.2
+	~/vscodium/vscodiumforcora/bin/codium --no-sandbox --force --install-extension eamodio.gitlens@13.5.0
+	~/vscodium/vscodiumforcora/bin/codium --no-sandbox --force --install-extension esbenp.prettier-vscode@10.1.0
+	~/vscodium/vscodiumforcora/bin/codium --no-sandbox --force --install-extension formulahendry.auto-close-tag@0.5.14
+	~/vscodium/vscodiumforcora/bin/codium --no-sandbox --force --install-extension formulahendry.auto-rename-tag@0.1.10
+	~/vscodium/vscodiumforcora/bin/codium --no-sandbox --force --install-extension humao.rest-client@0.25.1
+	~/vscodium/vscodiumforcora/bin/codium --no-sandbox --force --install-extension jeff-hykin.better-dockerfile-syntax@1.0.2
+	~/vscodium/vscodiumforcora/bin/codium --no-sandbox --force --install-extension jeff-hykin.better-shellscript-syntax@1.6.2
+	~/vscodium/vscodiumforcora/bin/codium --no-sandbox --force --install-extension maciekkoks.luvia-theme@0.1.24
+	~/vscodium/vscodiumforcora/bin/codium --no-sandbox --force --install-extension MartinJohns.inline-types@0.3.0
+	~/vscodium/vscodiumforcora/bin/codium --no-sandbox --force --install-extension mhutchie.git-graph@1.30.0
+	~/vscodium/vscodiumforcora/bin/codium --no-sandbox --force --install-extension ms-azuretools.vscode-docker@1.24.0
+	~/vscodium/vscodiumforcora/bin/codium --no-sandbox --force --install-extension naumovs.color-highlight@2.5.0
+	~/vscodium/vscodiumforcora/bin/codium --no-sandbox --force --install-extension Nixon.env-cmd-file-syntax@0.3.0
+	~/vscodium/vscodiumforcora/bin/codium --no-sandbox --force --install-extension PKief.material-icon-theme@4.30.1
+	~/vscodium/vscodiumforcora/bin/codium --no-sandbox --force --install-extension SonarSource.sonarlint-vscode@3.21.0
+	~/vscodium/vscodiumforcora/bin/codium --no-sandbox --force --install-extension wix.vscode-import-cost@3.3.0
 }
 
 if [ ! -d ~/workspace/cora-vscodium ]; then
   	firstRun
 else
-	~/vscodium/vscodiumforcora/codium --enable-features=UseOzonePlatform --ozone-platform=wayland --no-sandbox
+	~/vscodium/vscodiumforcora/bin/codium --enable-features=UseOzonePlatform --ozone-platform=wayland --no-sandbox
 fi
